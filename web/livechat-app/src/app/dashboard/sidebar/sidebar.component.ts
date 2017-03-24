c4befed274ebccc558bdb209556ba3903ed00fe4
Wojtek Przechodzeń <wprzecho@gmail.com>
2017-03-24 23:37:33 +0100

    sidebar styles

--- web/livechat-app/src/app/dashboard/sidebar/sidebar.component.ts
+++ web/livechat-app/src/app/dashboard/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  options = [
+    {label: 'CHATY', selected: true},
+    {label: 'AUKCJE'},
+    {label: 'USTAWIENIA'},
+    {label: 'STATYSTYKI'},
+    {label: 'WYLOGUJ'},
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
