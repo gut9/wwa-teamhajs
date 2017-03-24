0d42bebf5ba65e53125c22c7e4c8675a7d300a2e
Przemek Andrzejewski <przemyslaw.andrzejewski@exaco.pl>
2017-03-24 21:34:46 +0100

    Configured asgi server and channels.

--- api/DashboardSeller/api/views.py
+++ api/DashboardSeller/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from channels import Group
+from channels.auth import channel_session_user_from_http
+from channels.sessions import channel_session
 
-# Create your views here.
+
+@channel_session_user_from_http
+def ws_connect(message):
+    Group("%s" % message.user).add(message.reply_channel)
+    message.reply_channel.send({'accept': True})
+
+
+@channel_session
+def ws_receive(message):
+    print message
+
+@channel_session
+def ws_disconnect(message):
+    pass
