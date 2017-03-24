130a2ecad7153a44d4f3477f40efde6c6fa26b16
Radosław Nowak <rnowak@bluesoft.net.pl>
2017-03-25 00:41:31 +0100

    dodana metoda logujaca

--- api/DashboardSeller/api/urls.py
+++ api/DashboardSeller/api/urls.py
@@ -4,5 +4,6 @@ from api import views
 app_name = 'api'
 urlpatterns = [
     #url(r'^getRestaurant/(?P<id>\w{0,50})/$', views.GetRestaurant.as_view(), name='getRestaurant'),
-    url(r'^getUser/', views.GetUser.as_view(), name='getUser')
+    url(r'^getUser/', views.GetUser.as_view(), name='getUser'),
+    url(r'^login/', views.LoginPerform.as_view(), name='login')
 ]
--- api/DashboardSeller/api/views.py
+++ api/DashboardSeller/api/views.py
@@ -1,11 +1,13 @@
+import json
+
+import requests
+from api.models import SellerUser
+from api.serializers import SellerUserSerializer
 from channels import Group
 from channels.auth import channel_session_user_from_http
 from channels.sessions import channel_session
-from rest_framework.views import APIView
 from rest_framework.response import Response
-
-from api.models import SellerUser
-from api.serializers import SellerUserSerializer
+from rest_framework.views import APIView
 
 
 @channel_session_user_from_http
@@ -18,13 +20,13 @@ def ws_connect(message):
 def ws_receive(message):
     print message
 
+
 @channel_session
 def ws_disconnect(message):
     pass
 
 
 class GetUser(APIView):
-
     def get(self, request):
         user = SellerUser.objects.get()
         serializer = SellerUserSerializer(user, many=False)
@@ -34,3 +36,22 @@ class GetUser(APIView):
         user = SellerUser.objects.get()
         serializer = SellerUserSerializer(user, many=False)
         return Response(serializer.data)
+
+
+class LoginPerform(APIView):
+    def post(self, request):
+        login = request.POST["login"]
+        password = request.POST["password"]
+
+        # get token for user
+        authResponse = requests.get('https://ssl.allegro.pl/auth/oauth/token?grant_type=client_credentials',
+                                    auth=('a41f5b2a-8e87-4b8b-b6fe-74cc763720d7',
+                                          'bxbb2gFqCP1aM3kNPeptAWQMGz9gosbe9JCO1sqlp0BhY9G4UufpkXgsSFQYE545'))
+
+        accessToken = json.loads(authResponse.text)["access_token"]
+
+        loginData = {'access_token': accessToken, 'login': login, 'password': password}
+        loginResult = request.post('https://api.natelefon.pl/v1/allegro/login', loginData)
+
+        responseData = {'access_token': accessToken, 'userId': loginResult}
+        return Response(responseData)
