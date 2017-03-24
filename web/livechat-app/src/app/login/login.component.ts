1a65c8299cf20402066de364451758f01450721a
Patryk Gutenplan <pgutenplan@gmail.com>
2017-03-24 23:06:37 +0100

    Login form & md5 password

--- web/livechat-app/src/app/login/login.component.ts
+++ web/livechat-app/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Md5} from 'ts-md5/dist/md5';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  username = '';
+  password = '';
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    let hashPass = Md5.hashStr(this.password);
+    console.log("Username: " + this.username, "Password: " + this.password, "HashPass: " + hashPass);
   }
 
 }
