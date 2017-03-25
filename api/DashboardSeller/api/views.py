e1eec81c1be60d4b31b4a26d0ee96ad13ae9d7eb
Radosław Nowak <rnowak@bluesoft.net.pl>
2017-03-25 09:36:00 +0100

    aaaa

--- api/DashboardSeller/api/views.py
+++ api/DashboardSeller/api/views.py
@@ -3,6 +3,7 @@ import base64
 import hashlib
 
 import json
+import urllib
 
 import requests
 from api.models import SellerUser
@@ -52,11 +53,11 @@ class GetUser(APIView):
 
 
 class LoginPerform(APIView):
-    def post(self, request):
+    def get(self, request):
         login = request.POST["login"]
         passwd = request.POST["password"]
-        password_sha = hashlib.sha256(passwd)
-        password = base64.b64encode(str(password_sha))
+        password_sha = hashlib.sha256(passwd).digest()
+        password = base64.urlsafe_b64encode(password_sha)
 
         # get token for user
         auth_response = requests.get('https://ssl.allegro.pl/auth/oauth/token?grant_type=client_credentials',
@@ -65,18 +66,18 @@ class LoginPerform(APIView):
 
         access_token = json.loads(auth_response.text)["access_token"]
 
-        # login_data = {'access_token': access_token, 'userLogin': login, 'hashPass': password}
-        # login_result = requests.post('https://api.natelefon.pl/v1/allegro/login', data=login_data)
+        login_data = {'access_token': access_token, 'userLogin': login, 'hashPass': password}
+        login_result = requests.post('https://api.natelefon.pl/v1/allegro/login', data=login_data)
 
-        # if login_result.status_code == 401:
-        #     return Response(status=status.HTTP_401_UNAUTHORIZED)
-        # elif login_result.status_code == 403:
-        #     return Response(status=status.HTTP_403_FORBIDDEN)
-        # elif login_result.status_code == 400:
-        #     return Response(status=status.HTTP_400_BAD_REQUEST)
-        # else:
-        response_data = {'access_token': access_token, 'userId': login}
-        return Response(response_data, status=status.HTTP_200_OK)
+        if login_result.status_code == 401:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        elif login_result.status_code == 403:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        elif login_result.status_code == 400:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            response_data = {'access_token': access_token, 'userId': login}
+            return Response(response_data, status=status.HTTP_200_OK)
 
 
 class GetAuctions(APIView):
@@ -93,20 +94,22 @@ class GetAuctions(APIView):
 
 
 class ManageMessages(APIView):
-    def get(self, request):
-        if 'auctionId' not in request.GET.keys():
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        else:
-            auction_id = request.GET["auctionId"]
-
-        messages = Message.objects.filter(destOfferId=auction_id)
-        if 'userId' in request.GET.keys():
-            user_id = request.GET['userId']
-            messages = messages.filter(originUserId=user_id)
-
-        serializer = MessageSerializer(messages, many=True)
-
-        return Response(serializer.data)
+    # def get(self, request):
+    #     if 'auctionId' not in request.GET.keys() and 'targetUserId' not in request.GET.keys():
+    #         return Response(status=status.HTTP_400_BAD_REQUEST)
+    #
+    #     if 'auctionId' in request.GET.keys():
+    #         messages = Message.objects.filter(destOfferId=auction_id)
+    #     else:
+    #         messages = Message.objects.filter(destOfferId=auction_id)
+    #
+    #     if 'userId' in request.GET.keys():
+    #         user_id = request.GET['userId']
+    #         messages = messages.filter(originUserId=user_id)
+    #
+    #     serializer = MessageSerializer(messages, many=True)
+    #
+    #     return Response(serializer.data)
 
     def post(self, request):
         user_id = request.POST["originUserId"]
@@ -121,11 +124,12 @@ class ManageMessages(APIView):
 
 class HourStatisticsGetter(APIView):
     def get(self, request):
-        if 'offer_id' not in request.GET.keys():
+        if 'userId' not in request.GET.keys():
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        offer_id = request.GET["offer_id"]
-        messages = Message.objects.filter(destOfferId=offer_id)
+        user_id = request.GET["userId"]
+        user = SellerUser.objects.get(UserId=user_id)
+        offers = Offer.objects.filter(owner=user)
 
         messages_counter = 0
         stats = range(0, 23)
@@ -133,12 +137,13 @@ class HourStatisticsGetter(APIView):
         for i in range(0, 23):
             stats[i] = 0
 
-        for message in messages:
-            date = message.date
-            stats[date.hour] += 1.0
-            messages_counter += 1
+        for offer in offers:
+            offer_id = offer.offerId
+            messages = Message.objects.filter(offerId=offer_id)
 
-        for i in range(0, 23):
-            stats[i] /= messages_counter
+            for message in messages:
+                date = message.date
+                stats[date.hour] += 1
+                messages_counter += 1
 
         return Response(stats)
