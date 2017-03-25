9d079cd189f1d259f3bc57b7e82e303f49571eda
Wojtek Przechodzeń <wprzecho@gmail.com>
2017-03-25 03:40:38 +0100

    missing files

---
+++ web/livechat-app/src/app/chats/chat-sidebar/chat-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-sidebar',
+  templateUrl: './chat-sidebar.component.html',
+  styleUrls: ['./chat-sidebar.component.less']
+})
+export class ChatSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
